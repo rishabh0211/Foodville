cbc612cdb4d1b1231800bb22562795fbbdf80fb1
rishabh0211 <rishabh0211@gmail.com>
2020-09-15 19:15:59 +0530

    restaurant model

---
+++ server/models/Restaurant.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const RestaurantSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 40,
+      required: [true, "Restaurant name is required"],
+      trim: true
+    },
+    description: {
+      type: String,
+      minlength: 10,
+      maxlength: 200,
+      required: [true, "Restaurant description is required"],
+      trim: true
+    },
+    owner: [{ type: ObjectId, ref: "User" }],
+    meals: [{ type: ObjectId, ref: "Meal" }],
+    deletedAt: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Restaurant", RestaurantSchema);
