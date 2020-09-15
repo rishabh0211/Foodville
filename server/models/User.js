f84c623b578b5f8f8e33ee99b9a1cb56669ede21
rishabh0211 <rishabh0211@gmail.com>
2020-09-15 16:32:44 +0530

    User model created

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+      required: "Name is required"
+    },
+    email: {
+      type: String,
+      required: "Email is required",
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      validate(value) {
+        if (value.length < 7) {
+          throw new Error('Password length should be greater than 6!');
+        }
+        if (value.toLowerCase().includes('password')) {
+          throw new Error('Password should not contain the word password in it!');
+        }
+      }
+    },
+    isRestaurantOwner: Boolean,
+    blockedUsers: [{ type: ObjectId, ref: User }]
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
