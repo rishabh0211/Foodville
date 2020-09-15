797e2c528a59f1308d35b899c16d44e60632ce5a
rishabh0211 <rishabh0211@gmail.com>
2020-09-15 16:12:49 +0530

    express middlewares

--- server/app.js
+++ server/app.js
@@ -2,11 +2,23 @@ const path = require("path");
 const express = require('express');
 const mongoose = require("mongoose");
 require('./db/mongoose');
-
+const logger = require("morgan");
+const cors = require('cors');
+const helmet = require("helmet");
+const compression = require("compression");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = express();
 const port = process.env.PORT || 4000;
+
+if (!dev) {
+  app.use(helmet());
+  app.use(compression());
+}
+app.use(logger("combined"));
+app.use(cors({
+  credentials: true
+}));
 
 app.listen(port, () => {
   console.log(`server is up and running on port 4000`);
