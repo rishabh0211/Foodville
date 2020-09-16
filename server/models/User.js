13b2b9471d31c37c7e92de85b92ffcad78074a6f
rishabh0211 <rishabh0211@gmail.com>
2020-09-16 18:01:07 +0530

    added pre save and post save hooks in user model

--- server/models/User.js
+++ server/models/User.js
@@ -36,4 +36,22 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Hash the plain text password before saving
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+// setting correct message corresponding to error
+userSchema.post("save", function (error, doc, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    next(new Error('Email must be unique'));
+  } else {
+    next(error);
+  }
+});
+
 module.exports = mongoose.model("User", userSchema);
