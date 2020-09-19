7ffbb002e7c5a87de8422d9c236f2fe98187182d
rishabh0211 <rishabh0211@gmail.com>
2020-09-19 17:11:23 +0530

    added link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import StyledNavbar from "./styled/StyledNavbar";
 
 const Navbar = () => {
   return (
     <StyledNavbar>
       <div className="nav-container">
-        <div className="logo">
+        <Link to="/" className="logo">
           <h1 className="logo__text">FOOD</h1>
           <h3 className="logo__text-light">VILLE</h3>
-        </div>
+        </Link>
         <ul className="nav-options">
           <li className="nav-item">Sign Up</li>
           <li className="nav-item nav-login">Login</li>
