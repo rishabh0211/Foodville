46deed54b946081840d74ac00164ac14978c57bd
rishabh0211 <rishabh0211@gmail.com>
2020-09-19 11:19:35 +0530

    components structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from './components/Home';
 import GlobalStyle from './styles/globalStyles';
 
 ReactDOM.render(
-  <>
+  <Router>
     <GlobalStyle />
     <Home />
-  </>,
+  </Router>,
   document.getElementById('root')
 );
 
