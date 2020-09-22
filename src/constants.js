6e0781f0e7bd51bc8ba3d723c25267f9f68f9884
rishabh0211 <rishabh0211@gmail.com>
2020-09-23 03:16:44 +0530

    reverted api endpoint constant

--- src/constants.js
+++ src/constants.js
@@ -78,5 +78,5 @@ export const orderNextStaus = {
 };
 
 // to be removed
-export const API_ENDPOINT = "http://localhost:4000";
-// export const API_ENDPOINT = "";
+// export const API_ENDPOINT = "http://localhost:4000";
+export const API_ENDPOINT = "";
