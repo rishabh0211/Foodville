14a232fd23960e7aebcec6908299ab5c81255ddb
rishabh0211 <rishabh0211@gmail.com>
2020-09-23 03:28:48 +0530

    prefetching lazy loaded modules

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,9 +2,9 @@ import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import Loader from "../components/Loader";
 
-const RestaurantsList = lazy(() => import("../components/RestaurantsList"));
-const Restaurant = lazy(() => import("../components/Restaurant"));
-const Orders = lazy(() => import("../components/Orders"));
+const RestaurantsList = lazy(() => import(/* webpackPrefetch: true */ "../components/RestaurantsList"));
+const Restaurant = lazy(() => import(/* webpackPrefetch: true */ "../components/Restaurant"));
+const Orders = lazy(() => import(/* webpackPrefetch: true */ "../components/Orders"));
 const Login = lazy(() => import("../components/Login"));
 const Cart = lazy(() => import("../components/Cart"));
 
@@ -12,7 +12,7 @@ const AppRouter = () => {
   return (
     <Suspense fallback={<Loader />}>
       <Switch>
-        <Route path="/" exact component={Login}/>
+        <Route path="/" exact component={Login} />
         <Route path="/restaurants" component={RestaurantsList} />
         <Route path="/restaurant/:restaurantId" component={Restaurant} />
         <Route path="/orders" component={Orders} />
